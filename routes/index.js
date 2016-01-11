d159ae153a8f01454ade4099dcc28e32535261a0
fazle-abbas <fazle.abbas@roundcorner.com>
2016-01-11 19:16:21 +0530

    Only one index.ejs

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,9 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', {
-        title: 'Express Manage',
-        name: 'Fazle Abbas'
+        title: 'New Index Page',
+        username: 'Fazle Abbas',
+        messageFrom: 'Divya Ugale'
     });
 });
 
